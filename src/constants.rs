d3f6b320e83505ed34ec1bd48b789f6c707b1a2b
Alex-Stone-Github <stonealex145@gmail.com>
2023-01-03 21:23:13 -0600

    New library needs transpose

--- src/constants.rs
+++
@@ -1,3 +0,0 @@
-
-const LOOK_BACK: usize = 30;
-const TEXT_SIZE: usize = 26*2;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,38 @@
-mod model;
-mod constants;
 
 fn main() {
     println!("Hello, world!");
+}
+
+
+fn random_matrix(rows: usize, columns: usize) -> amath::Matrix {
+    let elements = (0..rows*columns).map(|_| rand::random()).collect();
+    amath::Matrix::new(rows, columns, elements)
+}
+struct Linear {
+    weight: amath::Matrix,
+    weight_grad: Option<amath::Matrix>,
+    bias: amath::Matrix,
+    bias_grad: Option<amath::Matrix>,
+    x: Option<amath::Matrix>,
+}
+impl Linear {
+    fn new(nins: usize, nouts: usize) -> Self {
+        Self {
+            weight: random_matrix(nouts, nins),
+            weight_grad: None,
+            bias: random_matrix(nouts, 1),
+            bias_grad: None,
+            x: None,
+        }
+    }
+    fn forward(&mut self, x: &amath::Matrix) -> amath::Matrix {
+        self.x = Some(x.clone());
+        let matmul = amath::Matrix::matmul(&self.weight, x).expect("x must have
+        a proper shape");
+        let addition = amath::Matrix::add(&matmul, &self.bias).expect("something
+        terrible has happened");
+        addition
+    }
+    fn backward(&mut self, pgrad: &amath::Matrix) -> amath::Matrix {
+    }
 }
